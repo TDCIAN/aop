57997d94bb2bf9684abf69bf739b1c45738e9edf
TDCIAN <tdcian71@gmail.com>
2021-08-15 15:57:14 +0900

    use aop

--- src/main/java/com/example/aop/controller/RestApiController.java
+++ src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,6 +1,9 @@
 package com.example.aop.controller;
 
+import com.example.aop.annotation.Decode;
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,11 +12,32 @@ public class RestApiController {
 
     @GetMapping("/get/{id}")
     public String get(@PathVariable Long id, @RequestParam String name) {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        // TODO
+
+        stopWatch.stop();
+        System.out.println("total time: "+stopWatch.getTotalTimeSeconds());
         return id+" "+name;
     }
 
     @PostMapping("/post")
     public User post(@RequestBody User user) {
+        return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+        // db logic
+        Thread.sleep(1000 * 2); // 2초
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user) {
+        System.out.println("put"+user);
         return user;
     }
 }
